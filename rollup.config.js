e1d9b2d5e416efdab3da20133f26f3c5ba65039c
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-06-01 00:22:30 +0100

    Updated github workflows and deps
    Fixed rollup conifg

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,16 @@
-import { terser } from 'rollup-plugin-terser'
-import multi from '@rollup/plugin-multi-entry'
+import terser from "@rollup/plugin-terser"
+import multi from "@rollup/plugin-multi-entry"
 
 export default [
-    {
-        input: {
-            include: [
-                'scripts/*.js',
-                'scripts/*/*.js'
-            ],
-            exclude: [
-                'scripts/token-action-hud-template.min.js']
-        },
-        output: {
-            format: 'esm',
-            file: 'scripts/token-action-hud-template.min.js'
-        },
-        plugins: [
-            terser({ keep_classnames: true, keep_fnames: true }),
-            multi()
-        ]
-    }
+  {
+    input: {
+      include: ["scripts/*.js", "scripts/*/*.js"],
+      exclude: ["scripts/token-action-hud-template.min.js"],
+    },
+    output: {
+      format: "esm",
+      file: "scripts/token-action-hud-template.min.js",
+    },
+    plugins: [terser({ keep_classnames: true, keep_fnames: true }), multi()],
+  },
 ]
